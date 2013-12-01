109d78a14f3a8a5375e65831e76058461afd6aa6
alifanuraniputri <alifanuraniputri@gmail.com>
2013-12-01 17:51:40 +0700

    Revert "index.php dan jadwal.php"
    This reverts commit 70bce7822cf06e82a23dbf3794f4d0136d0902ca.

--- index.php
+++ index.php
@@ -1,27 +1,8 @@
 <?php
 	include 'header.php';
 ?>
-<link rel="stylesheet" type="text/css" href="css/home.css">
 <div id="Content">
-		<div class="carousel">
-
-			  <ul class="panes">
-
-				<li>
-				  <img src="images/caro_consult.png" alt="">
-				</li>
-
-				<li>
-				  <img src="images/caro_reminder.png" alt="">
-				</li>
-
-				<li>
-				  <img src="images/caro_schedule.png" alt="">
-				</li>
-
-			  </ul>
-
-		</div>
+	CONTENTNYA HOME
 </div>
 
 <?php
